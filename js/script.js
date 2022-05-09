6949317c62e447d3cb9cf87d0386392919460664
nicolasgaldino <nicolasesmael1998@gmail.com>
2022-05-09 13:58:09 -0300

    Menu dropdown adicionado.

--- js/script.js
+++ js/script.js
@@ -7,20 +7,59 @@ function btnLogin() {
     i.addEventListener('click', alerta);
   });
   }
-  btnLogin();
+btnLogin();
   
-  function btnOrcamento() {
+function btnOrcamento() {
   const btnOrcamento = document.querySelector('.inicio-text button');
   btnOrcamento.addEventListener('click', () => {
     window.alert('Site em manuntenção.');
   });
   }
-  btnOrcamento();
+btnOrcamento();
+
+function initDrpodownMenu() {
+  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+  dropdownMenus.forEach(menu => {
+    ['touchstart', 'click'].forEach(userEvent => {
+      menu.addEventListener(userEvent, handleClick);
+    });
+  });
+
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.add('ativo');
+    clickFora(this, ['touchstart', 'click'], () => {
+      this.classList.remove('ativo');
+    });
+  }
+}
+initDrpodownMenu();
+
+function clickFora(element, events, callBack) {
+  const html = document.documentElement;
+  const fora = 'data-outside';
+  if(!element.hasAttribute(fora)) {
+    events.forEach(userEvent => {
+      html.addEventListener(userEvent, handleOutsideClick);
+    });
+    element.setAttribute(fora, '');
+  }
+  function handleOutsideClick(event) {
+    if(!element.contains(event.target)) {
+      element.removeAttribute(fora);
+      events.forEach(userEvent => {
+        html.addEventListener(userEvent, handleOutsideClick);
+      });
+      callBack();
+    }
+  }
+}
+clickFora();
   
-  function btnLigacao() {
+function btnLigacao() {
   const btnLigacao = document.querySelector('.contato button');
   btnLigacao.addEventListener('click', () => {
     window.alert('Site em manuntenção.');
   });
   }
-  btnLigacao();
+btnLigacao();
